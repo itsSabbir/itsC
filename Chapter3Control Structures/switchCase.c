f7fa00410dbf7bf2121f0d8c4424b6d7b856f2e0
itsSabbir <53710832+itsSabbir@users.noreply.github.com>
2024-09-16 10:40:05 -0400

    Update switchCase.c

--- Chapter3Control Structures/switchCase.c
+++ Chapter3Control Structures/switchCase.c
@@ -20,7 +20,7 @@ void faqs_and_troubleshooting(void);
 void recommended_tools_and_libraries(void);
 
 int main() {
-    printf("Expert-level Cheat Sheet: Control Structures (switch-case) in C\n\n");
+    printf("Cheat Sheet: Control Structures (switch-case) in C\n\n");
 
     overview_and_syntax();
     basic_switch_case();
